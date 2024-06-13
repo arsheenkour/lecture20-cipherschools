157e3250003fa1334f1345144629bdd39d1a9c00
arsheenkour <arsheenkour47@gmail.com>
2024-06-13 14:35:44 +0530

    first commit

---
+++ address.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int a;
+	int b;
+	float c;
+	char d;
+	cout<<"the address of a is "<<&a<<endl;
+	cout<<"the address of b is "<<&b<<endl;
+	cout<<"the address of c is "<<&c<<endl;
+	cout<<"the address of d is "<<&d<<endl;
+	
+	
+}
+
